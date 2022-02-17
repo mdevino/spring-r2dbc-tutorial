9975fde03315e86bca8208d97278a58d5efac4d3
Mateus Devino <mateus@mdevino.com>
2022-02-17 19:05:59 -0300

    Starter Project
    * R2DBC
    * H2 Database
    * Lombok

---
+++ src/main/java/com/mdevino/springr2dbctutorial/SpringR2dbcTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.mdevino.springr2dbctutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2dbcTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringR2dbcTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdevino/springr2dbctutorial/SpringR2dbcTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdevino.springr2dbctutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2dbcTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
